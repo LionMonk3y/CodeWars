82261067d757d9187768c63cdbf9b43a81ebf41e
LionMonk3y <jewgienij.walczykowski@gmail.com>
2023-10-02 21:09:25 +0200

    1st Day Of CodeWars Challenge

---
+++ kyu8/howGoodAreYouReally.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/5601409514fc93442500010b/javascript
+
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+// Note:
+
+// Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+
+function betterThanAverage(classPoints, yourPoints) {
+    const sum = classPoints.reduce((accumulator, current) => accumulator + current, 0);
+    const average = (sum + yourPoints) / (classPoints.length + 1);
+    return yourPoints > average;
+  }
---
+++ kyu8/transportationOnVcation.js
@@ -0,0 +1,30 @@
+/* https://www.codewars.com/kata/568d0dd208ee69389d000016/solutions/javascript */
+
+function rentalCarCost(d) {
+    return 40 * d - (d >= 7 ? 50 : d >= 3 ? 20 : 0);
+  }
+  
+  /* Second attempt
+  
+    let cost = 40 * d;
+  
+    if (d >= 7) {
+      cost -= 50;
+    } else if (d >= 3) {
+      cost -= 20;
+    }
+  
+    return cost;
+  }
+  */
+  
+  /* First attempt
+    
+  if ( d < 3){
+    return 40 * d
+  }else if( 3 <= d && d < 7){
+    return 40 * d - 20
+  }else if( d >= 7){
+    return 40 * d - 50
+  }
+  */
