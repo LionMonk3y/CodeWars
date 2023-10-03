80b049a16ec8da41a7aaac9c829772769e4cf037
LionMonk3y <jewgienij.walczykowski@gmail.com>
2023-10-03 19:58:06 +0200

    2nd Day Of CodeWars Challenge

---
+++ kyu8/calculateAverage.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/57a2013acf1fa5bfc4000921
+
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+function findAverage(array) {
+    return array.length ? array.reduce((x , y) => x + y, 0) / array.length : 0
+  }
