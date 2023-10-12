3f5234a60ee56fbbf42eff6b1f5dbfce6a93155a
LionMonk3y <jewgienij.walczykowski@gmail.com>
2023-10-12 20:53:16 +0200

    11th Day Of CodeWars Challenge

--- kyu7/highestAndLowest.js
+++ kyu7/highestAndLowest.js
@@ -1,4 +1,4 @@
-// https://www.codewars.com/kata/554b4ac871d6813a03000035/javascript
+// https://www.codewars.com/kata/554b4ac871d6813a03000035/javascript 
 
 // In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
 
