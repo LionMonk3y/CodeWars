9c669eae55aaa5f3e1c76df873b09590ec1fddd3
LionMonk3y <jewgienij.walczykowski@gmail.com>
2023-10-14 22:20:22 +0200

    13th Day Of CodeWars Challenge

---
+++ kyu7/shortestWord.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9/javascript
+
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+function findShort(s) {
+    const words = s.split(' ');
+    let shortestLength = Infinity;
+  
+    for (const word of words) {
+      const wordLength = word.length;
+      if (wordLength < shortestLength) {
+        shortestLength = wordLength;
+      }
+    }
+  
+    return shortestLength;
+  }
