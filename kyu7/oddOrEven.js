d488c5a99aee8f4f2af39c522e66b3e720055f40
LionMonk3y <jewgienij.walczykowski@gmail.com>
2023-10-25 23:42:45 +0200

    23rd Day Of CodeWars Challenge

---
+++ kyu7/oddOrEven.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5949481f86420f59480000e7/javascript
+
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+function oddOrEven(array) {
+  return array.reduce((a, b) => a + b, 0) % 2 === 0 ? "even" : "odd";
+}
