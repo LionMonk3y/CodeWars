446af233c4a65c98a69cc13e48b7a34e409051d0
LionMonk3y <jewgienij.walczykowski@gmail.com>
2023-11-13 21:31:36 +0100

    42nd Day Of CodeWars Challenge

---
+++ kyu7/checkTheExam.js
@@ -0,0 +1,23 @@
+// https://www.codewars.com/kata/5a3dd29055519e23ec000074/solutions/javascript
+
+// The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+
+// The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+
+// If the score < 0, return 0.
+
+function checkExam(array1, array2) {
+  let score = 0;
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      score += 4;
+    } else if (array2[i] === "") {
+      score += 0;
+    } else {
+      score -= 1;
+    }
+  }
+
+  return score < 0 ? 0 : score;
+}
