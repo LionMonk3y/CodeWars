ab4d41e9a5978d252cc2e9e1adde8d6fed134a97
LionMonk3y <jewgienij.walczykowski@gmail.com>
2023-12-01 23:40:52 +0100

    58th Day Of CodeWars Challenge

---
+++ kyu7/moneyMoneyMoney.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/563f037412e5ada593000114/javascript
+// Mr. Scrooge has a sum of money 'P' that he wants to invest. Before he does, he wants to know how many years 'Y' this sum 'P' has to be kept in the bank in order for it to amount to a desired sum of money 'D'.
+
+// The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly. After paying taxes 'T' for the year the new sum is re-invested.
+
+// Note to Tax: not the invested principal is taxed, but only the year's accrued interest
+
+function calculateYears(principal, interest, tax, desired) {
+  let years = 0;
+
+  while (principal < desired) {
+    const interestEarned = principal * interest;
+    const taxPaid = interestEarned * tax;
+    principal += interestEarned - taxPaid;
+    years++;
+  }
+
+  return years;
+}
