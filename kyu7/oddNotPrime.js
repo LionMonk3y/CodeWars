0a4b9741624b5e4f8dedd11b37b2c5822c9069c4
LionMonk3y <jewgienij.walczykowski@gmail.com>
2023-12-08 20:55:04 +0100

    65th Day Of CodeWars Challenge

---
+++ kyu7/oddNotPrime.js
@@ -0,0 +1,27 @@
+// https://www.codewars.com/kata/5a9996fa8e503f2b4a002e7a/javascript
+
+// For "x", determine how many positive integers less than or equal to "x" are odd but not prime. Assume "x" is an integer between 1 and 10000.
+
+// Example: 5 has three odd numbers (1,3,5) and only the number 1 is not prime, so the answer is 1
+
+// Example: 10 has five odd numbers (1,3,5,7,9) and only 1 and 9 are not prime, so the answer is 2
+
+function isPrime(num) {
+  if (num <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+function oddNotPrime(x) {
+  let count = 0;
+
+  for (let i = 1; i <= x; i += 2) {
+    if (!isPrime(i)) {
+      count++;
+    }
+  }
+
+  return count;
+}
